9d13ae3fd509610188ca8dc73677b7666ab50105
alpin33 <70812467+alpin33@users.noreply.github.com>
2020-10-23 10:01:51 +0300

    feat: solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
   let tmp;
+  while (str) {
+    for (brackets of bracketsConfig) {
+      str = str.replace(brackets.join(''), '')
+    }
+    if (str === tmp) {
+      return false;
+    }
+    tmp = str;
+  }
+  return str.length === 0;
 }
